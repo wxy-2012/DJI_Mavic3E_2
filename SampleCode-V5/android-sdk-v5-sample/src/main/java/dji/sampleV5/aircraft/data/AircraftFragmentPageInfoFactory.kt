2bb7d504022fd79c8267bb7b992e44990eaad2d8
WXY <804543632@qq.com>
2025-10-19 21:42:59 +0800

    first commit

---
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/data/AircraftFragmentPageInfoFactory.kt
@@ -0,0 +1,32 @@
+package dji.sampleV5.aircraft.data
+
+import dji.sampleV5.aircraft.R
+
+
+/**
+ * Class Description
+ *
+ * @author Hoker
+ * @date 2021/5/7
+ *F
+ * Copyright (c) 2021, DJI All Rights Reserved.
+ */
+class
+AircraftFragmentPageInfoFactory : IFragmentPageInfoFactory {
+
+    override fun createPageInfo(): FragmentPageItemList {
+        return FragmentPageItemList(R.navigation.nav_aircraft).apply {
+            items.add(FragmentPageItem(R.id.virtual_stick_page, R.string.item_virtual_stick_title, R.string.item_virtual_description))
+            items.add(FragmentPageItem(R.id.flight_record_page, R.string.item_flight_record_title, R.string.item_flight_record_description))
+            items.add(FragmentPageItem(R.id.flight_upgrade_page, R.string.item_upgrade_title, R.string.item_upgrade_description))
+            items.add(FragmentPageItem(R.id.flight_simulator_page, R.string.item_simulator_title, R.string.item_simulator_description))
+            items.add(FragmentPageItem(R.id.waypoint_v3_page, R.string.item_waypoint_title, R.string.item_waypoint_description))
+            items.add(FragmentPageItem(R.id.waypoint_v3_page, R.string.item_waypoint_title, R.string.item_waypoint_description))
+            items.add(FragmentPageItem(R.id.perception_page, R.string.item_perception_title, R.string.item_perception_description))
+            items.add(FragmentPageItem(R.id.uas_page, R.string.item_uas_title, R.string.item_uas_description))
+            items.add(FragmentPageItem(R.id.fly_safe_page, R.string.item_fly_safe_title, R.string.item_fly_safe_description))
+            items.add(FragmentPageItem(R.id.security_code_page, R.string.item_security_code_title, R.string.item_security_code_description))
+            items.add(FragmentPageItem(R.id.look_at_page, R.string.item_look_at_title, R.string.item_look_at_description))
+        }
+    }
+}
---
+++ SampleCode-V5/android-sdk-v5-sample/src/main/java/dji/sampleV5/aircraft/pages/UASRemoteFragment.kt
@@ -0,0 +1,33 @@
+package dji.sampleV5.aircraft.pages
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import dji.sampleV5.aircraft.R
+import dji.sampleV5.aircraft.databinding.FragUasPageBinding
+
+/**
+ * Description :无人机远程识别的演示入口页面
+ *
+ * @author: Byte.Cai
+ *  date : 2022/6/26
+ *
+ * Copyright (c) 2022, DJI All Rights Reserved.
+ */
+class UASRemoteFragment : DJIFragment() {
+    private var binding: FragUasPageBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragUasPageBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.btnChina?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_open_uas_chin_page)
+        }
+    }
+}
